81a95178aabf7bff38a1c0fa3c12a08d1ee89629
AbhisekKeshari <keshariabhisek6@gmail.com>
2025-06-16 00:08:28 +0530

    updated files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace `your-repo-name` with your GitHub repo name
 export default defineConfig({
-  base: '/AbhisekKeshari/',
+  base: '/AbhisekKeshari/', // ✅ repo name goes here
   plugins: [react()],
 })
